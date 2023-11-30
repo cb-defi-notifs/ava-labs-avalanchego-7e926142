vtamara/avalanchego

e2e: Update RestartSubnets to take subnets as a variadic argument

tests/fixture/testnet/local/network.go
@@ -878,7 +878,7 @@ func (ln *LocalNetwork) WriteSubnets(subnets []*testnet.Subnet) error {
 	return nil
 }
 
-func (ln *LocalNetwork) RestartSubnets(ctx context.Context, w io.Writer, subnets []*testnet.Subnet) error {
+func (ln *LocalNetwork) RestartSubnets(ctx context.Context, w io.Writer, subnets ...*testnet.Subnet) error {
 	for _, subnet := range subnets {
 		nodes, err := subnet.GetNodes(ln)
 		if err != nil {

cb-defi-notifs/ava-labs-avalanchego-7e926142

tests/fixture/tmpnet/local/network.go
@@ -892,7 +892,7 @@ func (ln *LocalNetwork) WriteSubnets(subnets []*tmpnet.Subnet) error {
 	return nil
 }
 
-func (ln *LocalNetwork) RestartSubnets(ctx context.Context, w io.Writer, subnets []*tmpnet.Subnet) error {
+func (ln *LocalNetwork) RestartSubnets(ctx context.Context, w io.Writer, subnets ...*tmpnet.Subnet) error {
 	for _, subnet := range subnets {
 		nodes, err := subnet.GetNodes(ln)
 		if err != nil {